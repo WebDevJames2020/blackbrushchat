384f520fd82394499f1ca900d2c41596c4076bf6
unknown <jamie_b12@hotmail.com>
2023-09-10 14:30:25 +0100

    second commit

--- calculator.js
+++ calculator.js
@@ -18,7 +18,7 @@ app.post("/", function (req, res) {
     res.send("Result: " + result);
   });
 
-app.listen(3000, function(){
-    console.log("Server started on port 3000")
-});
+//app.listen(3000, function(){
+//    console.log("Server started on port 3000")
+//});
 
